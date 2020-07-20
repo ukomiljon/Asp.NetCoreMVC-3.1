69b6df16e299a89c539c8e1473af7f028008f476
ukomiljon <ukomiljon@users.noreply.github.com>
2020-07-20 13:38:13 +0800

    middleware

--- WebGentle BookStore/WebGentle BookStore/Startup.cs
+++ WebGentle BookStore/WebGentle BookStore/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -33,25 +34,48 @@ namespace WebGentle_BookStore
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("Hello from my first middleware!");
+            //    await next();
+            //    await context.Response.WriteAsync("Hello from my first middleware!");
+            //});
+
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("Hello from my second middleware!");
+            //    await next();
+            //    await context.Response.WriteAsync("Hello from my second middleware!");
+
+            //});
+
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("Hello from my third middleware!");
+            //    await next();
+            //}); 
+            
+            
 
             app.UseRouting();
 
-            app.UseAuthorization();
-
+            
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.Map("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
             });
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.Map("/products", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello Products!");
+            //    });
+            //});
         }
     }
 }

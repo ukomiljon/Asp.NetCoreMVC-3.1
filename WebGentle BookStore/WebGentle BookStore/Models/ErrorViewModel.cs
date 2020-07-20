3dafd2e00514d983e89dc206f7901e5bf5fd3be1
ukomiljon <ukomiljon@users.noreply.github.com>
2020-07-20 12:52:46 +0800

    web

---
+++ WebGentle BookStore/WebGentle BookStore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebGentle_BookStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
